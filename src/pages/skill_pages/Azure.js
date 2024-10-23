b1c783ab9b843d7c70eb8b3c0cf06e94b9ce93ab
Julia <juliagolemo61@gmail.com>
2024-10-23 15:53:42 +0000

    azure_blob

--- src/pages/skill_pages/Azure.js
+++ src/pages/skill_pages/Azure.js
@@ -3,8 +3,7 @@ import { Link } from 'react-router-dom';
 import './Classes.css'; 
 
 const cards = [
-  { id: 'example', title: 'Azure wprowadzenie ch. 1', date: '2024-10-01', image: '/images/microsoft-azure.png'},
-  { id: 'example2', title: 'Azure Blob ch. 2', date: '2024-10-02', image: '/images/Blob-Storage-In-Azure.png' },
+  { id: 'azure_blob', title: 'Azure Blob ch. 2', date: '2024-10-02', image: '/images/Blob-Storage-In-Azure.png' },
 ];
 
 export default function Azure() {

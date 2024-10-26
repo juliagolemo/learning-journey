0aeff8be611a41ec273fa1cf1ff6e9f180654746
Julia <juliagolemo61@gmail.com>
2024-10-26 10:10:56 +0000

    Add Azure md to web app

--- src/pages/skill_pages/Azure.js
+++ src/pages/skill_pages/Azure.js
@@ -4,6 +4,7 @@ import './Classes.css';
 
 const cards = [
   { id: 'azure_blob', title: 'Azure Blob ch. 2', date: '2024-10-02', image: '/images/Blob-Storage-In-Azure.png' },
+  { id: 'azure_fundamentals', title: 'Azure Fundamentals', date: '2024-10-02', image: '/images/microsoft-azure.png' },
 ];
 
 export default function Azure() {

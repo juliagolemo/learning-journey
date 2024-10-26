120592a31f156fd3570ffc00976968083e670a90
Julia <juliagolemo61@gmail.com>
2024-10-26 18:55:13 +0000

    What is Azure

--- src/pages/skill_pages/Azure.js
+++ src/pages/skill_pages/Azure.js
@@ -6,6 +6,7 @@ const cards = [
   { id: 'azure_blob', title: 'Azure Blob ch. 2', date: '2024-10-02', image: '/images/Blob-Storage-In-Azure.png' },
   { id: 'azure_fundamentals', title: 'Azure Fundamentals', date: '2024-10-02', image: '/images/microsoft-azure.png' },
   { id: 'azure_app_service', title: 'Azure App Service', date: '2024-10-02', image: '/images/azure.png' },
+  { id: 'what_is_azure', title: 'What is Azure', date: '2024-10-26', image: '/images/Microsoft Azure Blog.webp' },
 ];
 
 export default function Azure() {

fab8bf4fa9bccb934a468dcfc02bbcb5b85cd251
Julia <juliagolemo61@gmail.com>
2024-10-28 18:08:02 +0000

    Add GoLang conditions

--- src/pages/skill_pages/GoLang.js
+++ src/pages/skill_pages/GoLang.js
@@ -6,6 +6,7 @@ const cards = [
   { id: 'golang_var', title: ' Variable: What is variable? Types (string, int, bool, float32,float64)', date: '2024-10-01', image: '/images/0_SoqCeEz9EctJBXKw.png' },
   { id: 'golang_func', title: ' Functions', date: '2024-10-01', image: '/images/golangheader.png' },
   { id: 'golang_loops', title: ' Loops ', date: '2024-10-01', image: '/images/golang-structs.webp' },
+  { id: 'golang_conditions', title: ' Conditions ', date: '2024-10-01', image: '/images/golang-pic.png' },
 ];
 
 export default function GoLang() {

3ca2b803a2e1234790d16ef687835f7f48f09dd0
Julia <juliagolemo61@gmail.com>
2024-11-07 13:06:35 +0000

    Delete Excel card

--- src/pages/skill_pages/Excel.js
+++ src/pages/skill_pages/Excel.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import './Classes.css'; 
 
 const cards = [
-  { id: 'example', title: 'Excel', date: '2024-10-01', image: '/images/excel_1.jpg'},
   { id: 'excel_if', title: 'Excel IF function & conditions', date: '2024-10-01', image: '/images/excel_1.jpg'},
   { id: 'excel_pivot_table', title: 'Pivot Table', date: '2024-11-07', image: '/images/pivot_table.png'},
 

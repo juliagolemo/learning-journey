2a077f9b13d6878c33b018754b45e9effc76f4f5
Julia <juliagolemo61@gmail.com>
2024-11-17 12:08:08 +0000

    Refactor Technologies.js

--- src/pages/Technologies.js
+++ src/pages/Technologies.js
@@ -1,40 +1,42 @@
 import React from 'react';
 import CircleSkillsList from '../components/CircleSkillsList';
 
+const cards_path = '/images/cards/';
+
 const technologies = [
   {
     name: 'Excel',
-    image: '/images/cards/excel/Excel.png',
+    image: `${cards_path}excel/Excel.png`,
     path: 'excel',
     description: 'Microsoft Excel is a spreadsheet program.',
   },
   {
     name: 'Azure',
-    image: '/images/cards/azure/Azure.png',
+    image: `${cards_path}azure/Azure.png`,
     path: 'azure',
     description: 'Microsoft Azure is a cloud computing service.',
   },
   {
     name: 'GoLang',
-    image: '/images/cards/golang/gofer.png',
+    image: `${cards_path}golang/gofer.png`,
     path: 'golang',
     description: 'GoLang is a programming language created by Google.',
   },
   {
     name: 'Git',
-    image: '/images/cards/git/git.png',
+    image: `${cards_path}git/git.png`,
     path: 'git',
     description: 'Git is a distributed version control system.',
   },
   {
     name: 'German',
-    image: '/images/cards/german/flag.png',
+    image: `${cards_path}german/flag.png`,
     path: 'german',
     description: 'My progress in learning German.',
   },
   {
     name: 'Web Development',
-    image: '/images/cards/web_development/webdevelopment.webp',
+    image: `${cards_path}web_development/webdevelopment.webp`,
     path: 'webdevelopment',
     description: 'Process of a Web Development.',
   }
